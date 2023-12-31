8b32fc0b77dea12c60907d3b62950455694a070c
SHANTANU KUMAR <shantanukuame23291143@gmail.com>
2023-12-31 12:24:28 +0530

    first commit

---
+++ src/main/java/Travel/SoftwareDesignTaskApplication.java
@@ -0,0 +1,13 @@
+package Travel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareDesignTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareDesignTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Travel/SoftwareDesignTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package Travel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareDesignTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
